fc0e7284bed3d04341aa23756a123cb4802ab7d0
Maryam Arab <maryam.arab83@gmail.com>
2017-11-16 14:31:22 -0500

    mid Nov

--- StrategyTracker/js/index.js
+++ StrategyTracker/js/index.js
@@ -25,6 +25,11 @@ if (typeof window !== 'undefined' && window.angular) {
 
     myapp.controller('MainCtrl', function ($scope, StrategyService) {
         "use strict";
+        // $scope.allVariables=[ {name: 'code', val: null},
+        //     {name: 'referenceCode', val: null},
+        //     {name: 'failure', val: null},
+        //     {name: 'system', val: null},
+        //      ];
         $scope.accordion = {
             current: null
         };
@@ -37,43 +42,61 @@ if (typeof window !== 'undefined' && window.angular) {
             $scope.currentStrategy = strategies[0].strategies[0];
             $scope.selectedStrategy=$scope.currentStrategy;
             $scope.activeLines = [];
-            // $scope.strategyChanged = function () {
-            //     $scope.selectedStrategy = $scope.selectedStrategy;
-            //     //$scope.selectedStrategy=$scope.selectedStrategy;
-            // }
-
-            $scope.variables = $scope.currentStrategy.parameters;
-
+            $scope.allVariables=[];
             let interpreter = new models.Interpreter($scope.strategies);
+            let varNames=[];
+
+
             $scope.selectedStrategy  = $scope.currentStrategy;
-            // initialize the application
-            let execObj = interpreter.init($scope.currentStrategy.name);
-            $scope.currentStrategy = execObj.currentStrategy;
-            $scope.currentStatement = execObj.currentStatement;
-            $scope.activeLines = execObj.activeLines;
+            $scope.strategyChanged = function () {
+
+                $scope.execObj = interpreter.init($scope.selectedStrategy.strategies[0]);
+                $scope.currentStrategy = $scope.execObj.currentStrategy;
+                $scope.currentStatement = $scope.execObj.currentStatement;
+                $scope.variables = $scope.execObj.variables;
+                for(var i=0; i<$scope.strategies.length; i++){
+                    $scope.extractVariables($scope.strategies[i]);
+                }
+                for(var i = 0; i<varNames.length; i++){
+                   $scope.allVariables.push({"name":varNames[i], "val": null});
+                }
+            }
+
+            $scope.extractVariables= function(strategy){
+
+                for(var i= 0 ; i<strategy.statements.length; i++) {
+                    if (strategy.identifier !== undefined && varNames.indexOf(strategy.identifier.replace (/'/g, "")) === -1)
+                        varNames.push(strategy.identifier.replace (/'/g, ""))
+                    let x = strategy.statements[i];
+                    if (x.identifier !== undefined && varNames.indexOf( x.identifier.replace (/'/g, "")) === -1)
+                        varNames.push( x.identifier.replace (/'/g, ""));
+                    if (x.statements !== undefined) {
+                        $scope.extractVariables(x);
+                    }
+                }
+
+            };
 
             $scope.reset= function () {
-                execObj = interpreter.reset();
-                interpreter = new models.Interpreter(strategies);
-                execObj = interpreter.init("localizeFailure");
-                $scope.strategy = execObj.currentStrategy;
-                $scope.currentStatement = execObj.currentStatement;
-                $scope.statements = $scope.strategy.statements;
-                angular.forEach($scope.variables, function(val, key) {
-                    val.val = null;
-                });
+                // execObj = interpreter.reset();
+                // interpreter = new models.Interpreter(strategies);
+                // execObj = interpreter.init("localizeFailure");
+                // $scope.strategy = execObj.currentStrategy;
+                // $scope.currentStatement = execObj.currentStatement;
+                // $scope.statements = $scope.strategy.statements;
+                // angular.forEach($scope.variables, function(val, key) {
+                //     val.val = null;
+                // });
             };
 
             $scope.nextStatement = function () {
-                execObj = interpreter.execute();
-                $scope.currentStatement = execObj.currentStatement;
-                $scope.activeLines = execObj.activeLines;
-               // $scope.currentStrategy = execObj.currentStrategy;
-                if (execObj === null) return;
-                if ($scope.currentStrategy.name !== execObj.currentStrategy.name) {
-                    $('#' + execObj.currentStrategy.name).collapse('show');
+                $scope.execObj = interpreter.execute();
+                if ($scope.execObj === null) return;
+                $scope.variables = $scope.execObj.variables;
+                if ($scope.currentStrategy.name !== $scope.execObj.currentStrategy.name) {
+                    $('#' + $scope.execObj.currentStrategy.name).collapse('show');
                     $('#' + $scope.currentStrategy.name).collapse('hide');
-                    $scope.currentStrategy = execObj.currentStrategy;
+                    $scope.currentStrategy = $scope.execObj.currentStrategy;
                     if($scope.strategy !== undefined)
                         $scope.statements = $scope.strategy.statements;
                     //$('#' +$scope.strategy.name).collapse('show');
@@ -81,36 +104,44 @@ if (typeof window !== 'undefined' && window.angular) {
             };
 
             $scope.prevStatement = function () {
-                execObj = interpreter.goBack();
-                if (execObj === null) return;
-                if ($scope.currentStrategy.name !== execObj.currentStrategy.name) {
-                    $('#' + execObj.currentStrategy.name).collapse('show');
+                $scope.execObj = interpreter.goBack();
+                if ($scope.execObj === null) return;
+                $scope.variables = $scope.execObj.variables;
+                if ($scope.currentStrategy.name !== $scope.execObj.currentStrategy.name) {
+                    $('#' + $scope.execObj.currentStrategy.name).collapse('show');
                     $('#' + $scope.currentStrategy.name).collapse('hide');
-                    $scope.currentStrategy = execObj.currentStrategy;
+                    $scope.currentStrategy = $scope.execObj.currentStrategy;
                     $scope.statements = $scope.currentStrategy.statements;
                 }
-                $scope.currentStatement = execObj.currentStatement;
-                $scope.activeLines = execObj.activeLines;
-                $scope.currentStrategy = execObj.currentStrategy;
+                $scope.currentStatement = $scope.execObj.currentStatement;
+                $scope.activeLines = $scope.execObj.activeLines;
+                $scope.currentStrategy = $scope.execObj.currentStrategy;
             };
+            $scope.outerStatement = function () {
+                $scope.execObj = interpreter.refreshStatement(false);
 
-            $scope.chooseNextStatement = function ($event) {
-                let chosenStatement = "";
-                if ($scope.activeLines.length > 1)
-                {
-                let currentTarget = $event.currentTarget.innerHTML;
-                for (var x=0; x<$scope.activeLines.length; x++) {
-                    var line = $scope.activeLines[x];
-                    if (currentTarget.includes(line))
-                        chosenStatement = line;
-                }
-                execObj = interpreter.execute(chosenStatement);
-                if (execObj === null) return;
-                $scope.currentStrategy = execObj.currentStrategy;
-                $scope.currentStatement = execObj.currentStatement;
-                $scope.activeLines = execObj.activeLines;
-                }
-            };
+            }
+            $scope.innerStatement = function () {
+                $scope.execObj = interpreter.refreshStatement(true);
+            }
+
+            // $scope.chooseNextStatement = function ($event) {
+            //     let chosenStatement = "";
+            //     if ($scope.activeLines.length > 1)
+            //     {
+            //     let currentTarget = $event.currentTarget.innerHTML;
+            //     for (var x=0; x<$scope.activeLines.length; x++) {
+            //         var line = $scope.activeLines[x];
+            //         if (currentTarget.includes(line))
+            //             chosenStatement = line;
+            //     }
+            //     execObj = interpreter.execute(chosenStatement);
+            //     if (execObj === null) return;
+            //     $scope.currentStrategy = execObj.currentStrategy;
+            //     $scope.currentStatement = execObj.currentStatement;
+            //     $scope.activeLines = execObj.activeLines;
+            //     }
+            // };
         });
 
     });
--- StrategyTracker/js/models.js
+++ StrategyTracker/js/models.js
@@ -4,23 +4,22 @@ var clone = require('clone');
 
 class Interpreter {
 
-
     constructor(strategies) {
         this.executionStack = [];
         this.historyBackward = [];
         this.strategies = strategies;
     }
 
-    init(name) {
-        let initExecutionContext = new FunctionExecContext(name, this.strategies);
+    init(currentStrategy) {
+
+        let initExecutionContext = new FunctionExecContext(currentStrategy.name, this.strategies);
         //initExecutionContext.activeLines[0] = initExecutionContext.strategy.text;
         this.executionStack.push(initExecutionContext);
 
         return {
-            currentStatement : initExecutionContext.strategy.text,
-            currentStrategy: initExecutionContext.strategy,
             executionStack: this.executionStack,
             activeLines : initExecutionContext.activeLines,
+            currentStrategy:initExecutionContext.strategy
         }
     }
     reset(){
@@ -28,7 +27,6 @@ class Interpreter {
         this.historyBackward.splice(0,this.historyBackward.length);
         this.executionStack.splice(0,this.executionStack.length);
         this.updateWizard(wizardDiv);
-        this.dirty= false;
     }
     findStatementByText(currentStrat,statementText){
         for(var i=0; i<currentStrat.statements.length; i++){
@@ -52,8 +50,9 @@ class Interpreter {
         let currentExecutionContext = this.executionStack.pop();
         if(currentExecutionContext === undefined)
             return null;
-        if(currentStatementText == undefined)
+        if(currentStatementText == undefined )//&& currentExecutionContext.blocks.length != 0
         {
+
             currentExecutionContext= currentExecutionContext.getNextStatement(currentExecutionContext.pc);
         }
         else{
@@ -62,13 +61,37 @@ class Interpreter {
             currentExecutionContext.activeLines.push(currentExecutionContext.currentStatement.text);
             currentExecutionContext.pc = currentExecutionContext.currentStatement;
         }
+        if(currentExecutionContext.pc.type =="if")
+            currentExecutionContext.selectionNeeded = true;
+        else
+            currentExecutionContext.selectionNeeded = false;
+
         this.executionStack.push((currentExecutionContext));
         return {
             currentStatement: currentExecutionContext.pc,
             currentStrategy: currentExecutionContext.strategy,
             executionStack: this.executionStack,
             activeLines :currentExecutionContext.activeLines,
+            selectionNeeded : currentExecutionContext.selectionNeeded,
+            variables:currentExecutionContext.variables,
         };
+    }
+    refreshStatement(isTrue){
+        if(!isTrue){
+            let currentExecObj = clone(this.executionStack.pop());
+            currentExecObj.selectionNeeded= false;
+            this.executionStack.push(currentExecObj);
+            currentExecObj = currentExecObj.blocks[0].shift();
+            return this.execute(currentExecObj.text);
+        }
+        else if (isTrue)
+        {
+            let currentExecObj = clone(this.executionStack.pop());
+            currentExecObj.blocks[0].unshift(currentExecObj.pc.statements);
+            this.executionStack.push(currentExecObj);
+            currentExecObj = currentExecObj.blocks[0].shift();
+            return this.execute(currentExecObj.text);
+        }
     }
 
     updateWizard( wizardDiv) {
@@ -84,34 +107,49 @@ class Interpreter {
     }
 
     goBack() {
+        let tempExecObj = this.executionStack[this.executionStack.length - 1];
         let stack = this.historyBackward.pop();
+
         if(stack === undefined)
             return null;
         this.executionStack = stack;
-        let executionContext = this.executionStack[this.executionStack.length - 1];
+        console.log(this.executionStack[this.executionStack.length - 1].blocks[0]);
+        if(this.executionStack[0].blocks.length===0)
+            this.executionStack[this.executionStack.length - 1].blocks.push(tempExecObj.prevTemp);
+        else
+            this.executionStack[this.executionStack.length - 1].blocks[0].unshift(tempExecObj.prevTemp);
+        let currentExecutionContext = this.executionStack[this.executionStack.length - 1];
         // let wizardDiv = document.getElementById("wizard");
         // if(executionContext.strategy.displayName != wizardDiv.lastElementChild.lastChild.data)
         //     this.updateWizard(wizardDiv)
-
+        if(currentExecutionContext.pc.type =="if" )
+            currentExecutionContext.selectionNeeded = true;
+        else
+            currentExecutionContext.selectionNeeded = false;
         return {
-            currentStatement:  executionContext.pc,
-            currentStrategy: executionContext.strategy,
+            currentStatement:  currentExecutionContext.pc,
+            currentStrategy: currentExecutionContext.strategy,
             executionStack: this.executionStack,
-            activeLines: executionContext.activeLines,
+            activeLines: currentExecutionContext.activeLines,
+            selectionNeeded : currentExecutionContext.selectionNeeded,
+            variables:currentExecutionContext.variables,
         };
     }
 }
+
+
 class FunctionExecContext {
-
-    constructor(strategyName, strategies) {
-
+    constructor(currentStrategy, strategies) {
         this.strategies = strategies;
-        this.strategy = this.findStrategy(strategyName);
+        this.strategy = this.findStrategy(currentStrategy);
         this.pc = this.strategy;
-        this.i = 0;
-       // this.conditionalCounter=0;
         this.activeLines = [];
         this.activeLines.push(this.strategy.text);
+        this.blocks = [];
+        this.blocks.unshift(clone(this.strategy.statements));
+        this.variables=[];
+        this.selectionNeeded= false;
+        this.prevTemp=this.strategy;
     }
     findStrategy(strategyname) {
         return this.strategies.find(function(strategy) {
@@ -120,30 +158,28 @@ class FunctionExecContext {
     }
 
     getNextStatement(currentStatement) {
+        this.prevTemp = currentStatement;
         this.activeLines=[];
         //||currentStatement.type == "foreach" || currentStatement.type == "until"
-        if(currentStatement.type == "if"  ) {
-            // if(currentStatement.satements.length >0)
-            // {
-            //     let newFec = new FunctionExecContext();
-            // }
-            this.pc = currentStatement.statements[0];
-            this.activeLines.push(currentStatement.statements[0].text);
-            this.activeLines.push(this.strategy.statements[this.i].text);
-            this.i++;
-        }
-        else if (currentStatement.type == "strategy" ){
-            this.i =0;
-            this.pc = this.strategy.statements[this.i];
+        if (currentStatement.type == "strategy" ){
+            // this.blocks.unshift(clone(currentStatement.statements));
+            this.pc = this.blocks[0].shift();
             this.activeLines.push(this.pc.text);
-            this.i++;
         }
+        else if(currentStatement.type == "if"  ) {
+            if(currentStatement.statements !== undefined && currentStatement.statements.length>0)
+                for(var i=currentStatement.statements.length-1; i>=0; i--){
+                    this.blocks[0].unshift(clone(currentStatement.statements[i]));
+                }
+            this.pc = this.blocks[0].shift();
+            this.activeLines.push(this.pc.text);
 
+        }
         else if(currentStatement.type =="return"){
-             if (currentStatement.query.type== "call") {
-                 let newFec = new FunctionExecContext(currentStatement.query.name, this.strategies);
-                 return newFec;
-             }
+            if (currentStatement.query.type== "call") {
+                let newFec = new FunctionExecContext(currentStatement.query.name, this.strategies);
+                return newFec;
+            }
         }
         else if (currentStatement.type=="do") {
             if(currentStatement.call !== undefined){
@@ -151,63 +187,27 @@ class FunctionExecContext {
                 return newFec;
             }
         }
-        else if(currentStatement.type == "foreach" ) {
-            this.pc = currentStatement.statements[0];
-            this.activeLines.push(currentStatement.statements[0].text);
-            this.activeLines.push(this.strategy.statements[this.i].text);
+        else if(currentStatement.type =="foreach"){
+            if(currentStatement.statements !== undefined && currentStatement.statements.length>0)
+                for(var i=currentStatement.statements.length-1; i>=0; i--){
+                    this.blocks[0].unshift(clone(currentStatement.statements[i]));
+                }
 
+            this.pc = this.blocks[0].shift();
+            this.activeLines.push(this.pc.text);
         }
         else{
-            this.pc = this.strategy.statements[this.i];
+            if(currentStatement.statements !== undefined)
+                this.blocks[0].unshift(clone(currentStatement.statements[0]));
+            this.pc = this.blocks[0].shift();
             this.activeLines.push(this.pc.text)
-            this.i++;
         }
+        if(this.pc.identifier !== undefined)
+            this.variables.push({"name" :this.pc.identifier , "val":null});
+
         return this;
     }
-
 }
-// execute() {
-//     let wizardDiv = document.getElementById("wizard");
-//
-//     if(this.executionStack.length) {
-//         this.historyBackward.push(clone(this.executionStack)); // take a snapshot from our current executionStack to our history
-//     }
-//     let executionContext = this.executionStack.pop();
-//     if(executionContext === undefined)
-//         return null;
-//     if(!this.dirty && executionContext.pc.nextStrategy.length) {
-//
-//         let nextExecutionContext = new FunctionExecContext(this.findStrategy(executionContext.pc.nextStrategy));
-//         this.dirty = true;
-//         this.executionStack.push(executionContext);
-//         this.executionStack.push(nextExecutionContext);
-//
-//         this.updateWizard(wizardDiv);
-//         return {
-//             currentStatement: nextExecutionContext.pc,
-//             currentStrategy: nextExecutionContext.strategy,
-//             executionStack: this.executionStack,
-//         };
-//     } else {
-//         this.dirty = false;
-//         let nextStatement = executionContext.getNextStatement(lineNum);
-//         if(nextStatement.successor==undefined || nextStatement.successor=="undefined")
-//         {
-//             this.dirty = true;
-//             executionContext = this.executionStack.pop();
-//             if(executionContext === undefined)
-//                 return null;
-//             nextStatement= executionContext.getNextStatement(executionContext.pc.lineNum);
-//         }
-//         this.executionStack.push(executionContext);
-//         this.updateWizard(wizardDiv);
-//         return {
-//             currentStatement: nextStatement,
-//             currentStrategy: executionContext.strategy,
-//             executionStack: this.executionStack,
-//         };
-//     }
-// }
 
 module.exports = {
     Interpreter: Interpreter
